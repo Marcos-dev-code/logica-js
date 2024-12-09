c2c106744703bacead08713a208df1e52cf8ee9b
Marcos Roberto <mr.codeengineer@gmail.com>
2024-12-09 10:40:32 -0300

    novo exercicio completado, ainda precisa ser estilizado

--- Ex avulsos/Ex 05/script.js
+++ Ex avulsos/Ex 05/script.js
@@ -1,1 +1,19 @@
+function calcularIdade(){
+    let idade = document.getElementById("idade").value
+    idade = parseFloat(idade);
+    let resposta = document.getElementById("resposta")
 
+    if(idade >= 5 && idade <=10){
+        resposta.innerHTML="Você ainda é uma <strong>Criança</strong>"
+    }else if(idade<=4){
+        resposta.innerHTML="Não presta mentir, digite sua idade verdadeira <strong>seu frango</strong>"
+    }else if (idade>10 && idade <15){
+        resposta.innerHTML="Você é um(a) <strong>adolescente</strong>"
+    }else if (idade>14 && idade <18){
+        resposta.innerHTML="Você é um <strong>jovem</strong>"
+    }else if(idade>17 && idade<60){
+        resposta.innerHTML="você é um <strong>adulto</strong>"
+    }else{
+        resposta.innerHTML="Você já é considerado uma pessoa de <strong>idade avançada</strong>"
+    }
+}
